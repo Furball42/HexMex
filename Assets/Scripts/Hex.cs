8dbb35976d76067178e087c7045d1515feaaddf9
Marius Meyer <mariusmeyer42@gmail.com>
2021-01-31 16:52:56 +0200

    Initial commit
    - Hex Map
    - Rhombus to Rectangular
    - Elevations (basic)

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Hex is responsible for holding the data regarding a specific hex on the map, including position, neighbours etc.
+public class Hex
+{
+    public readonly int Q;
+    public readonly int R;
+    public readonly int S;
+    public float Elevation;
+
+    static readonly float WIDTH_MULTIPLIER = Mathf.Sqrt(3) / 2;
+
+    public Hex(int q, int r){
+
+        Q = q;
+        R = r;
+        S = -q-r;
+    }
+
+    //Return world space position
+    public Vector3 Position(){
+        
+        float radius = 1f;
+        float height = radius * 2;
+        float width = WIDTH_MULTIPLIER * height;
+
+        float horizontalOffset = width;
+        float verticalOffset = height * 0.75f;
+        
+
+        return new Vector3(
+            horizontalOffset * (Q + R / 2f),
+            0,
+            verticalOffset * R
+        );        
+    }
+}
---
+++ Assets/Scripts/HexMap.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//HexMap is responsible for building the tactical map. This includes size and shape, and handling the terrain.
+
+public class HexMap : MonoBehaviour
+{
+    public int NumCols = 12;
+    public int NumRows = 8;
+    public GameObject HexPrefab;
+
+    //TODO: move these out to its class/array
+    public Material MatLvl0;
+    public Material MatLvl1;
+    public Material MatLvl2;
+
+
+    private Hex[,] hexes;
+    private Dictionary<Hex, GameObject> hexToGameObjectMap;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void Update()
+    {
+    }
+
+    public void GenerateMap() {
+
+        hexes = new Hex[NumCols, NumRows];
+        hexToGameObjectMap = new Dictionary<Hex, GameObject>();
+
+        for (int r = 0; r < NumRows; r++)
+        {
+            //offset to create rectangle from rhombus
+            int r_offset = (int)Mathf.Floor(r/2);
+
+            for (int q = -r_offset; q < NumCols - r_offset; q++) {
+
+                Hex hex = new Hex(q, r);
+                // hexes[q, r] = hex;
+                
+                //set initial params
+                hex.Elevation = 0f;
+
+                GameObject hexGO = Instantiate(HexPrefab, hex.Position(), Quaternion.identity, this.transform);
+
+                hexToGameObjectMap[hex] = hexGO;
+                
+                //add perlin noise vir elevation                
+                Vector2 noiseOffset = new Vector2( Random.Range(0f, 1f), Random.Range(0f, 1f) );
+                float noiseSample = Mathf.PerlinNoise(q + noiseOffset.x, r + noiseOffset.y);
+
+                //bounds the perlin noise to 0 - 1
+                if(noiseSample > 1)
+                    noiseSample = 1;
+                if(noiseSample < 0)
+                    noiseSample = 0;
+
+                //assign elevation to hex
+                hex.Elevation += noiseSample;
+
+                // Debug.Log(string.Format("{0},{1} E:{2}", q, r, hex.Elevation));
+
+                //text mesh element
+                hexGO.GetComponentInChildren<TextMesh>().text = string.Format("{0},{1}", q, r);
+            }
+
+        }
+
+        UpdateTileVisuals();
+    }
+
+    public void UpdateTileVisuals(){
+
+        foreach(KeyValuePair<Hex, GameObject> entry in hexToGameObjectMap)
+        {
+            GameObject hexGO = entry.Value;
+            Hex hex = entry.Key;
+
+            MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
+
+            //set terrain material according to height
+            //TODO: Update this to it's own class
+            if(hex.Elevation >= 0.7f)
+                mr.material = MatLvl2;
+            else if(hex.Elevation >= 0.55f && hex.Elevation < 0.7f)
+                mr.material = MatLvl1;
+            else
+                mr.material = MatLvl0;
+
+        }
+    }
+}
+    
